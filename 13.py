bed6be5b221e0aa3412dac9dfdbe73ad42cd8aac
Svyatoslav Usachev <susachev@google.com>
2022-12-13 11:29:10 +0000

    Add files via upload

---
+++ 13.py
@@ -0,0 +1,5 @@
+from sys import stdin
+
+#(t:=lambda x:isinstance(x,int)) and (cmp:=lambda s,a,b:((a>b)-(a<b) if t(a) else s(s,a,[b])) if t(b) else (s(s,[a],b) if t(a) else next((x for c,d in zip(a,b) if (x:=s(s,c,d))),0) or s(s,len(a),len(b)))) and print(sum([i+1 for i,v in enumerate(cmp(cmp,eval(s[0]),eval(s[1]))<0 for p in stdin.read().split('\n\n') for s in [p.split()]) if v]))
+
+(t:=lambda x:isinstance(x,int)) and (cmp:=lambda s,a,b:((a>b)-(a<b) if t(a) else s(s,a,[b])) if t(b) else (s(s,[a],b) if t(a) else next((x for c,d in zip(a,b) if (x:=s(s,c,d))),0) or s(s,len(a),len(b)))) and (x:=sum((cmp(cmp,eval(s),[[2]])<0)+1j*(cmp(cmp,eval(s),[[6]])<0) for s in stdin.read().split())) and print((x.real+1)*(x.imag+2))
