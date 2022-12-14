3da698cd8520c4a3ab7028d81300cdbfcdadedf1
Svyatoslav Usachev <susachev@google.com>
2022-12-14 22:04:28 +0000

    Update 10.py

--- 10.py
+++ 10.py
@@ -1,4 +1,5 @@
 from itertools import accumulate
 from sys import stdin
 
+# Part 2
 print(*('\n'[i%40:]+'.#'[-2<i%40-r<2] for i,r in enumerate(accumulate(stdin.read().split(),lambda r,c:r+(not c.isalpha() and int(c)),initial=1))))
