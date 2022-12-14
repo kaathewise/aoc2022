d1052a8dfebf07e157f22eb0b0b93b17e8639ce2
Svyatoslav Usachev <susachev@google.com>
2022-12-14 22:07:55 +0000

    Update 8.py

--- 8.py
+++ 8.py
@@ -2,4 +2,5 @@ from functools import reduce
 from operator import mul
 from sys import stdin
 
+# Part 2
 ((m:=[[int(c) for c in s[:-1]] for s in stdin.readlines()]),(t:=[*zip(*m)]),print(max(reduce(mul,(next((k+1 for k,x in enumerate(v) if x>=m[i][j]), len(v)) for v in (m[i][j-1::-1],m[i][j+1:],t[j][i-1::-1],t[j][i+1:]))) for i in range(1,98) for j in range(1,98))))
