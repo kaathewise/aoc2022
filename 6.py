61c3ba2866102fd6fbf64cba69b94779161549ab
Svyatoslav Usachev <susachev@google.com>
2022-12-14 22:07:23 +0000

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,4 +1,7 @@
 from sys import stdin
 
+# Part 1
 #(s:=stdin.read()) and print(next(i+4 for i in range(len(s)) if len(set(s[i:i+4]))==4))
+
+# Part 2
 (s:=stdin.read()) and print(next(i+14 for i in range(len(s)) if len(set(s[i:i+14]))==14))
