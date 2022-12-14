e4bbec33cd8c78c4cf06a0d288be6cbc3884df48
Svyatoslav Usachev <susachev@google.com>
2022-12-14 22:06:29 +0000

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,4 +1,7 @@
 from sys import stdin
 
+# Part 1
 #print(sum((l:=len(s)//2) and (p:=ord((set(s[:l])&set(s[l:])).pop())-96) and p+(p<0)*58  for s in stdin.readlines()))
+
+# Part 2
 print((s:=(l.strip() for l in stdin.readlines())) and sum((p:=ord((set(a)&set(b)&set(c)).pop())-96) and p+(p<0)*58  for a,b,c in zip(s,s,s)))
