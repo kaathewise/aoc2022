9a45682389516719d8b1be8fdd6c78b5ea174608
Svyatoslav Usachev <susachev@google.com>
2022-12-14 22:06:05 +0000

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,5 +1,7 @@
 from sys import stdin
 
+# Part 1
 #print(sum((b-a+4)%3*3+b for r in stdin.readlines() for a,b in [(ord(r[0])-64, ord(r[2])-87)]))
-print(sum((a+t+1)%3+1+t*3 for r in stdin.readlines() for a,t in [(ord(r[0])-64, ord(r[2])-88)]))
 
+# Part 2
+print(sum((a+t+1)%3+1+t*3 for r in stdin.readlines() for a,t in [(ord(r[0])-64, ord(r[2])-88)]))
