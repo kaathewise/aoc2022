4c0580cb3f175e8b6560b2fd1d8aeab92fc83799
Svyatoslav Usachev <susachev@google.com>
2022-12-15 08:28:13 +0000

    Add files via upload

---
+++ 15.py
@@ -0,0 +1,4 @@
+from re import findall
+from sys import stdin
+
+(n:=map(int,findall('-?\d+',stdin.read()))) and (s:=[(a,b,abs(a-c)+abs(b-d)) for a,b,c,d in zip(n,n,n,n)]) and print(next(x*4000000+y for u,v,w in s for a in [-1,1] for b in [-1,1] for k in range(w+2) for x,y in [(u+a*k,v+b*(w+1-k))] if 0<=x<=4000000 and 0<=y<=4000000 and all(abs(x-t[0])+abs(y-t[1])>t[2] for t in s)))
