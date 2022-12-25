d30909c9f886a314b4fe23719c3162198521b7bf
Svyatoslav Usachev <susachev@google.com>
2022-12-25 12:20:54 +0000

    Update 25.py

--- 25.py
+++ 25.py
@@ -1,3 +1,3 @@
 from sys import stdin
 
-(n:=lambda x:x and n(x[:-1])*5+'=-012'.find(x[-1])-2 or 0) and (s:=lambda x:x and ((q:=(x+2)%5-2),s((x-q)//5)+'012=-'[q])[1] or '') and print(s(sum(n(l[:-1]) for l in stdin.readlines())))
+(n:=lambda x:x and n(x[:-1])*5+'=-012'.find(x[-1])-2 or 0) and (s:=lambda x:x and s((x+2)//5)+'=-012'[(x+2)%5] or '') and print(s(sum(n(l[:-1]) for l in stdin.readlines())))
