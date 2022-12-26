041a96ba7c1f8ac959e44a0db83cea9da6de883f
Svyatoslav Usachev <susachev@google.com>
2022-12-26 15:02:33 +0000

    Update 4.py

--- 4.py
+++ 4.py
@@ -2,7 +2,7 @@ from re import findall
 from sys import stdin
 
 # Part 1
-#print((n:=map(int,findall(r'\d+',stdin.read()))) and sum((a==c) or (b==d) or (a<c)==(b>d) for (a,b,c,d) in zip(n,n,n,n)))
+# print((n:=map(int,findall(r'\d+',stdin.read()))) and sum((a==c) or (b==d) or (a<c)==(b>d) for (a,b,c,d) in zip(n,n,n,n)))
 
 # Part 2
 print((n:=map(int,findall(r'\d+',stdin.read()))) and sum(max(a,c)<=min(b,d) for a,b,c,d in zip(n,n,n,n)))
