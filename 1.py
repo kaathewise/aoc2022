83c67dda6ce37fbcb35e6f79583c30124daafa92
Svyatoslav Usachev <susachev@google.com>
2022-12-26 14:58:55 +0000

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,4 +1,7 @@
 from sys import stdin
 
+# Part 1
+# print(max(sum(map(int,e.split())) for e in stdin.read().split('\n\n')))
+
 # Part 2
 print(sum(sorted([sum(map(int,e.split())) for e in stdin.read().split('\n\n')])[-3:]))
