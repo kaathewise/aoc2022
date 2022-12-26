f271ccbfd7898ae2c2dd062262b0577a071a3b5c
Svyatoslav Usachev <susachev@google.com>
2022-12-26 15:09:24 +0000

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,5 +1,8 @@
 from re import findall
 from sys import stdin
 
+# Part 1
+# ((i:=stdin.read().split('\n\n')),(m:=[[*filter(str.isalpha,s[::-1])] for c,s in enumerate(zip(*i[0].split('\n'))) if c%4==1]),(t:=map(int,findall(r'\d+',i[1]))),[(m[b-1].extend(m[a-1][-k:][::-1]),[m[a-1].pop() for _ in range(k)]) for k,a,b in zip(t,t,t)],print(''.join(x[-1] for x in m)))
+
 # Part 2
-((i:=stdin.read().split('\n\n')),(m:=[[*filter(str.isalpha,reversed(s))] for c,s in enumerate(zip(*i[0].split('\n'))) if c%4==1]),(t:=map(int,findall(r'\d+',i[1]))),[(m[b-1].extend(m[a-1][-k:]),[m[a-1].pop() for _ in range(k)]) for k,a,b in zip(t,t,t)],print(''.join(x[-1] for x in m)))
+((i:=stdin.read().split('\n\n')),(m:=[[*filter(str.isalpha,s[::-1])] for c,s in enumerate(zip(*i[0].split('\n'))) if c%4==1]),(t:=map(int,findall(r'\d+',i[1]))),[(m[b-1].extend(m[a-1][-k:]),[m[a-1].pop() for _ in range(k)]) for k,a,b in zip(t,t,t)],print(''.join(x[-1] for x in m)))
