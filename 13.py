9d12ebe8a69594d61de27aa489b3208e651697d9
Svyatoslav Usachev <susachev@google.com>
2022-12-26 15:31:03 +0000

    Update 13.py

--- 13.py
+++ 13.py
@@ -1,7 +1,7 @@
 from sys import stdin
 
 # Part 1
-#(t:=lambda x:isinstance(x,int)) and (cmp:=lambda s,a,b:((a>b)-(a<b) if t(a) else s(s,a,[b])) if t(b) else (s(s,[a],b) if t(a) else next((x for c,d in zip(a,b) if (x:=s(s,c,d))),0) or s(s,len(a),len(b)))) and print(sum([i+1 for i,v in enumerate(cmp(cmp,eval(s[0]),eval(s[1]))<0 for p in stdin.read().split('\n\n') for s in [p.split()]) if v]))
+# (t:=lambda x:isinstance(x,int)) and (C:=lambda a,b:((a>b)-(a<b) if t(a) else C(a,[b])) if t(b) else (C([a],b) if t(a) else next((x for c,d in zip(a,b) if (x:=C(c,d))),0) or C(len(a),len(b)))) and print(sum([i+1 for i,v in enumerate(C(eval(s[0]),eval(s[1]))<0 for p in stdin.read().split('\n\n') for s in [p.split()]) if v]))
 
 # Part 2
-(t:=lambda x:isinstance(x,int)) and (cmp:=lambda s,a,b:((a>b)-(a<b) if t(a) else s(s,a,[b])) if t(b) else (s(s,[a],b) if t(a) else next((x for c,d in zip(a,b) if (x:=s(s,c,d))),0) or s(s,len(a),len(b)))) and (x:=sum((cmp(cmp,eval(s),[[2]])<0)+1j*(cmp(cmp,eval(s),[[6]])<0) for s in stdin.read().split())) and print((x.real+1)*(x.imag+2))
+(t:=lambda x:isinstance(x,int)) and (C:=lambda a,b:((a>b)-(a<b) if t(a) else C(a,[b])) if t(b) else (C([a],b) if t(a) else next((x for c,d in zip(a,b) if (x:=C(c,d))),0) or C(len(a),len(b)))) and (x:=sum((C(eval(s),[[2]])<0)+1j*(C(eval(s),[[6]])<0) for s in stdin.read().split())) and print((x.real+1)*(x.imag+2))
